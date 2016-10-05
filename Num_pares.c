c0dfee234780a50773b5d1222dbaf832e7499ee5
Gr2_srk <gvjohan99@gmail.com>
2016-10-05 14:36:36 -0500

    Update Num_pares.c

--- Num_pares.c
+++ Num_pares.c
@@ -5,10 +5,10 @@ int main (int argc,char* argv []) //Funcion principial
 
     int a=0, b=0;
     printf("\n\t %c%c==============================================%c%c",201,205,205,187);
-	printf("\n\t %c                    Bienvenido                  %c",186,186);
+    printf("\n\t %c                    Bienvenido                  %c",186,186);
     printf("\n\t %c                    Soy Skynet                  %c",186,186);
     printf("\n\t %c                  >>moriras c:<<                %c",186,186);
-	printf("\n\t %c Este programa te ayudar%c a verificar si tus    %c",186,160,186);
+    printf("\n\t %c Este programa te ayudar%c a verificar si tus    %c",186,160,186);
     printf("\n\t %c               n%cmeros son m%cltiplos            %c",186,163,163,186);
     printf("\n\t %c%c==============================================%c%c",200,205,205,188);
 
